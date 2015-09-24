0590d1a68aa8b5b62d36db949da2314b104d9392
Ryan Yep <ryan.yep@gmail.com>
2015-09-23 21:22:06 -0500

    added tables - before migration

---
+++ db/migrate/20150924020945_create_tweets.rb
@@ -0,0 +1,11 @@
+class CreateTweets < ActiveRecord::Migration
+  def change
+    create_table :tweets do |t|
+      t.string :post
+      t.integer :user_id
+      t.belongs_to :user, index: true, foreign_key: true
+
+      t.timestamps null: false
+    end
+  end
+end
